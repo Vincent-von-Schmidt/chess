c27ac3da9bac9ce563229278147d99899bbfe335
Vincent-von-Schmidt <dev@liongray.com>
2025-04-14 11:42:14 +0200

    cleaned up filestructure and added tests for Fen and Color classes

--- src/main/kotlin/hwr/oop/Main.kt
+++ src/main/kotlin/hwr/oop/Main.kt
@@ -1,21 +1,6 @@
 package hwr.oop
 
-enum class Color {
-  WHITE, BLACK
-}
-
-data class Fen (val notation: String) {
-  val piecePlacement: List<String> = notation.split(" ").elementAt(0).split("/")
-  val activeColor: Color = when (notation.split(" ").elementAt(1)) {
-    "w" -> Color.WHITE
-    "b" -> Color.BLACK
-    else -> Color.WHITE // if not given asume white starts
-  }
-  val castle: String = notation.split(" ").elementAt(2)
-  val enpassant: String = notation.split(" ").elementAt(3)
-  val halfmoves: Int = notation.split(" ").elementAt(4).toInt()
-  val fullmoves: Int = notation.split(" ").elementAt(5).toInt()
-}
+import hwr.oop.fen.Fen
 
 fun main() {
   val fen: Fen = Fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2")
