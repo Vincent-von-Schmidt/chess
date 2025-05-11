23012cf28c88fd1a77f2e0d0fba16794a7c09e45
Vincent-von-Schmidt <dev@liongray.com>
2025-05-11 14:04:20 +0200

    fixed errors in generateRank

--- src/main/kotlin/group4/chess/board/Board.kt
+++ src/main/kotlin/group4/chess/board/Board.kt
@@ -3,42 +3,37 @@ import group4.chess.location.*
 
 fun main() {
     val board = Board()
-    // println("${board.root} -> ${board.root.location.file}${board.root.location.rank}")
-    // println("${board.root.right} -> ${board.root.right?.location?.file}${board.root.right?.location?.rank}")
-    // println("${board.root.right?.left} -> ${board.root.right?.left?.location?.file}${board.root.right?.left?.location?.rank}")
-    // println("${board.root.bottom} -> ${board.root.bottom?.location?.file}${board.root.bottom?.location?.rank}")
-    // println("${board.root.top} -> ${board.root.top?.location?.file}${board.root.top?.location?.rank}")
-    // println("${board.root.right?.bottom} -> ${board.root.right?.bottom?.location?.file}${board.root.right?.bottom?.location?.rank}")
-
-    var tmp: Field = board.root
-    for (i in 0..7) {
-        println("${tmp.location.file}${tmp.location.rank}")
-        println("${tmp.bottom?.location?.file}${tmp.bottom?.location?.rank}")
-        println("------")
-
-        if (i < 7)
-            tmp = tmp.right!!
-    }
 }
 
 class Board {
     val root: Field = generateFields()
 
     private fun generateRank(rankId: Int, lastRank: Field?): Field {
-        var newField = Field(Location(rankId, File.values()[0]))
-        val root: Field = newField
-        var lastRank: Field? = lastRank
-        for (file in 2..8) {
-            newField.right = Field(Location(rankId, File.values()[file-1]))
-            newField.right!!.left = newField
+        var currentFiled = Field(Location(rankId, File.values()[0])) // first Field of Rank + moving pointer to the current Field
+        val root: Field = currentFiled // stationary pointer to first Field of Rank
+        var topField: Field? = lastRank // moving pointer to the Field above the current Field
 
-            if (lastRank != null) {
-                newField.top = lastRank
-                newField.top!!.bottom = newField
-                lastRank = newField.right!!
+        for (file in 2..9) {
+
+            val isTopField: Boolean = topField != null
+            val isNotLastFile: Boolean = file <= 8
+
+            if (isTopField) {
+                currentFiled.top = topField
+                topField!!.bottom = currentFiled
             }
 
-            newField = newField.right!!
+            // new field on the right of current
+            if (isNotLastFile) {
+                val neoField = Field(Location(rankId, File.values()[file-1]))
+                currentFiled.right = neoField
+                neoField.left = currentFiled
+                currentFiled = neoField
+            }
+
+            if (isTopField && isNotLastFile) {
+                topField = topField!!.right!!
+            }
         }
         return root
     }
@@ -47,10 +42,10 @@ class Board {
         var firstRank: Field? = null
         var lastRank: Field? = null
 
-        for (y in 0..7) {
-            lastRank = generateRank(y, lastRank)
+        for (rank in 1..8) {
+            lastRank = generateRank(rank, lastRank)
 
-            if (y == 0) {
+            if (rank == 1) {
                 firstRank = lastRank
             }
         }
