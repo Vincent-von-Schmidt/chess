eb9a9fa1bcb4318f5a3acfa1636a7ed2d0b62940
Vincent-von-Schmidt <dev@liongray.com>
2025-05-12 11:52:21 +0200

    fixed currentFiled -> currentField

--- src/main/kotlin/group4/chess/board/Board.kt
+++ src/main/kotlin/group4/chess/board/Board.kt
@@ -5,8 +5,8 @@ class Board {
     val root: Field = generateFields()
 
     private fun generateRank(rankId: Int, lastRank: Field?): Field {
-        var currentFiled = Field(Location(rankId, File.values()[0])) // first Field of Rank + moving pointer to the current Field
-        val root: Field = currentFiled // stationary pointer to first Field of Rank
+        var currentField = Field(Location(rankId, File.values()[0])) // first Field of Rank + moving pointer to the current Field
+        val root: Field = currentField // stationary pointer to first Field of Rank
         var topField: Field? = lastRank // moving pointer to the Field above the current Field
 
         for (file in 2..9) {
@@ -15,16 +15,16 @@ class Board {
             val isNotLastFile: Boolean = file <= 8
 
             if (isTopField) {
-                currentFiled.top = topField
-                topField!!.bottom = currentFiled
+                currentField.top = topField
+                topField!!.bottom = currentField
             }
 
             // new field on the right of current
             if (isNotLastFile) {
                 val neoField = Field(Location(rankId, File.values()[file-1]))
-                currentFiled.right = neoField
-                neoField.left = currentFiled
-                currentFiled = neoField
+                currentField.right = neoField
+                neoField.left = currentField
+                currentField = neoField
             }
 
             if (isTopField && isNotLastFile) {
