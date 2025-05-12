40488651cb4739962186d2821f1ba0ab9cc33ae4
tombrt265 <tom@familie-bielert.de>
2025-05-12 12:47:07 +0200

    add games folder

--- src/main/kotlin/hwr/oop/group4/chess/core/utils/Constants.kt
+++ src/main/kotlin/hwr/oop/group4/chess/core/utils/Constants.kt
@@ -2,8 +2,8 @@ package hwr.oop.group4.chess.core.utils
 
 object Constants {
     const val TESTNUMBERS = 1000000
-    const val GAMESTESTFILE = "gamesTest.csv"
-    const val GAMESFILE = "games.csv"
+    const val GAMESTESTFILE = "games/gamesTest.csv"
+    const val GAMESFILE = "games/games.csv"
     const val STARTING_POSITION = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 }
