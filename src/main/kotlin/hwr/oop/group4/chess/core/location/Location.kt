3c5422ffa0900acba5972321be6da4095ce39e02
tombrt265 <tom@familie-bielert.de>
2025-05-15 12:23:53 +0200

    remove unnecessary imports

--- src/main/kotlin/hwr/oop/group4/chess/core/location/Location.kt
+++ src/main/kotlin/hwr/oop/group4/chess/core/location/Location.kt
@@ -1,7 +1,5 @@
 package hwr.oop.group4.chess.core.location
 
-import hwr.oop.group4.chess.core.location.File
-
 data class Location (val file: File, val rank: Int) {
     val description = "${file}${rank}"
 }
