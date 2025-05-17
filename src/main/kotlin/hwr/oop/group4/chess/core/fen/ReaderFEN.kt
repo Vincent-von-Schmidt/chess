39ac33037602cd1057871aa408480824cfad4e22
tombrt265 <tom@familie-bielert.de>
2025-05-17 20:26:09 +0200

    add to ReaderFENTest

--- src/main/kotlin/hwr/oop/group4/chess/core/fen/ReaderFEN.kt
+++ src/main/kotlin/hwr/oop/group4/chess/core/fen/ReaderFEN.kt
@@ -3,23 +3,52 @@ package hwr.oop.group4.chess.core.fen
 import hwr.oop.group4.chess.core.pieces.Color
 
 data class ReaderFEN (val notation: String) {
-    val piecePlacement: List<String> = notation.split(" ").elementAt(0).split("/")
-    val activeColor: Color = when (notation.split(" ").elementAt(1)) {
-        "w" -> Color.WHITE
-        "b" -> Color.BLACK
-        else -> throw IllegalArgumentException("Not a valid color")
+
+    /*
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq c6 0 2"
+    "1                                           2 3    4  5 6"
+    1: piece placement for each row
+        (capital letters for white, lowercase for black)
+        (numbers for empty squares)
+    2: active color (white or black)
+    3: castling availability
+        K/k: king side
+        Q/q: queen side
+    4: en passant target square
+        ("-" if none)
+    5: half move clock
+    6: full move number
+     */
+
+    private val parts: List<String> = notation.split(" ")
+
+    init {
+        if (parts.size != 6) throw EmptyFenException()
     }
-    val castle: String = notation.split(" ").elementAt(2)
-    val enpassant: String = notation.split(" ").elementAt(3)
-    val halfmoves: Int = notation.split(" ").elementAt(4).toInt()
-    val fullmoves: Int = notation.split(" ").elementAt(5).toInt()
+
+    fun getPiecePlacement(): List<String> = parts.elementAt(0).split("/")
+    fun getActiveColor(): Color {
+        return when (val activeColor = parts.elementAt(1)) {
+            "w" -> Color.WHITE
+            "b" -> Color.BLACK
+            else -> throw InvalidColorException(activeColor)
+        }
+    }
+    fun getCastle(): String = notation.split(" ").elementAt(2)
+    fun getEnPassant(): String = notation.split(" ").elementAt(3)
+    fun getHalfMoves(): Int = notation.split(" ").elementAt(4).toInt()
+    fun getFullMoves(): Int = notation.split(" ").elementAt(5).toInt()
 }
 
-/* "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq c6 0 2"
-groß weiß, klein schawarz,
-zahl:freie felder,
-w oder b für wessen zug es ist,
-castlezüge KQkq
-c6 ein en passant ziel feld
-50züge remis regel zähler, (halbzüge)
-zugzähler */
+class EmptyFenException : Exception(
+    """
+    FEN string is empty
+    """.trimIndent()
+)
+
+class InvalidColorException(activeColor: String) : Exception(
+    """
+    Invalid color: $activeColor
+    Active color must be either 'w' or 'b'
+    """.trimIndent()
+)
--- src/test/kotlin/hwr/oop/group4/chess/core/fen/ReaderFENTest.kt
+++ src/test/kotlin/hwr/oop/group4/chess/core/fen/ReaderFENTest.kt
@@ -8,58 +8,104 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 
 class ReaderFENTest : AnnotationSpec() {
 
-     @Test
-     fun `piece placement list is created correctly`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-         val fenOb = ReaderFEN(notationExample)
-         assertThat(fenOb.piecePlacement).isEqualTo(listOf("rnbqkbnr", "pp1ppppp", "8", "2p5", "4P3", "8", "PPPP1PPP", "RNBQKBNR"))
-     }
+  @Test
+  fun `piece placement list is created correctly`() {
+    // Given
+    val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
 
-     @Test
-     fun `active color is read as white`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-         val fenOb = ReaderFEN(notationExample)
-         assertThat(fenOb.activeColor).isEqualTo(Color.WHITE)
-     }
+    // When
+    val fenOb = ReaderFEN(notationExample)
 
-     @Test
-     fun `active color is read as black`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR b KQkq c6 0 2"
-         val fenOb = ReaderFEN(notationExample)
-         assertThat(fenOb.activeColor).isEqualTo(Color.BLACK)
-     }
+    // Then
+    assertThat(fenOb.getPiecePlacement()).isEqualTo(listOf("rnbqkbnr", "pp1ppppp", "8", "2p5", "4P3", "8", "PPPP1PPP", "RNBQKBNR"))
+  }
 
-     @Test
-     fun `active color throws an error on wrong color`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR t KQkq c6 0 2"
-         assertThatThrownBy { ReaderFEN(notationExample) }.isInstanceOf(IllegalArgumentException::class.java)
-     }
+  @Test
+  fun `active color is read as white or black`() {
+    // Given
+    val notationExample1 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+    val notationExample2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR b KQkq c6 0 2"
 
-     @Test
-     fun `castle is read correctly`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-         val fenOb = ReaderFEN(notationExample)
-         assertThat(fenOb.castle).isEqualTo("KQkq")
-     }
+    // When
+    val fenOb = ReaderFEN(notationExample1)
+    val fenOb2 = ReaderFEN(notationExample2)
 
-     @Test
-     fun `en passant is read correctly`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-         val fenOb = ReaderFEN(notationExample)
-         assertThat(fenOb.enpassant).isEqualTo("c6")
-     }
+    // Then
+    assertThat(fenOb.getActiveColor()).isEqualTo(Color.WHITE)
+    assertThat(fenOb2.getActiveColor()).isEqualTo(Color.BLACK)
+  }
 
-     @Test
-     fun `halfmoves are read correctly`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-         val fenOb = ReaderFEN(notationExample)
-         assertThat(fenOb.halfmoves).isEqualTo(0)
-     }
+  @Test
+  fun `active color throws an error on wrong color`() {
+    // Given
+    val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR t KQkq c6 0 2"
+    val fenOb = ReaderFEN(notationExample)
 
-     @Test
-     fun `fullmove are read correctly`() {
-         val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
-         val fenOb = ReaderFEN(notationExample)
-         assertThat(fenOb.fullmoves).isEqualTo(2)
-     }
+
+
+    // Then
+    assertThatThrownBy { fenOb.getActiveColor() }
+      .isInstanceOf(InvalidColorException::class.java)
+      .hasMessageContaining("Invalid color: t")
+  }
+
+  @Test
+  fun `castle is read correctly`() {
+    // Given
+    val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+    val fenOb = ReaderFEN(notationExample)
+
+    // When
+    val castle = fenOb.getCastle()
+
+    // Then
+    assertThat(castle).isEqualTo("KQkq")
+  }
+
+  @Test
+  fun `en passant is read correctly`() {
+    // Given
+    val notationExample = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+    val fenOb = ReaderFEN(notationExample)
+
+    // When
+    val enPassant = fenOb.getEnPassant()
+
+    // Then
+    assertThat(enPassant).isEqualTo("c6")
+  }
+
+  @Test
+  fun `half moves are read correctly`() {
+    // Given
+    val notationExample1 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
+    val notationExample2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 1 2"
+    val fenOb1 = ReaderFEN(notationExample1)
+    val fenOb2 = ReaderFEN(notationExample2)
+
+    // When
+    val halfMoves1 = fenOb1.getHalfMoves()
+    val halfMoves2 = fenOb2.getHalfMoves()
+
+    // Then
+    assertThat(halfMoves1).isEqualTo(0)
+    assertThat(halfMoves2).isEqualTo(1)
+  }
+
+  @Test
+  fun `full moves are read correctly`() {
+    // Given
+    val notationExample1 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 0"
+    val notationExample2 = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 1"
+    val fenOb1 = ReaderFEN(notationExample1)
+    val fenOb2 = ReaderFEN(notationExample2)
+
+    // When
+    val fullMoves1 = fenOb1.getFullMoves()
+    val fullMoves2 = fenOb2.getFullMoves()
+
+    // Then
+    assertThat(fullMoves1).isEqualTo(0)
+    assertThat(fullMoves2).isEqualTo(1)
+  }
 }
