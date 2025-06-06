608c1f7d90a3e964790d0375574b92ed8abd5de4
tombrt265 <tom@familie-bielert.de>
2025-06-06 11:02:45 +0200

    add recentFen to game (for three move repetition rule)

--- src/main/kotlin/hwr/oop/group4/chess/core/utils/Constants.kt
+++ src/main/kotlin/hwr/oop/group4/chess/core/utils/Constants.kt
@@ -2,8 +2,6 @@ package hwr.oop.group4.chess.core.utils
 
 object Constants {
   const val TEST_NUMBER = 1000000
-  const val GAMES_FILE_TEST = "games/gamesTest.csv"
-  const val GAMES_FILE = "games/games.csv"
   const val STARTING_POSITION =
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
   const val EMPTY_BOARD =
